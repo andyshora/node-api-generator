2062882710222eff46aedc255a9faaecf7accf29
Andy Shora <andyshora@gmail.com>
2014-01-19 17:13:40 +0000

    Converted parse connection to promise based responses

---
+++ api/lib/parse-REST-functions.js
@@ -0,0 +1,77 @@
+// instantiate
+var APP_ID = 'uX77K4d7z4PnWODj6Rr1YkJwAIsFHQp4aMeDa4mm';
+var REST_API_KEY = 'xIfE9g6vDdZMvA6nYxF0kfuVnbyswUO2AlY159T7';
+
+var Kaiseki = require('./kaiseki'),
+	Q = require('q');
+
+var parse = new Kaiseki(APP_ID, REST_API_KEY);
+
+module.exports = {
+	getObject: function (className, id) {
+		var deferred = Q.defer();
+		parse.getObject(className, id, {}, function(err, response, body, success) {
+			if (err) {
+				deferred.reject(err);
+			} else if (body.error) {
+				deferred.reject(body);
+			} else {
+				deferred.resolve(body);
+			}
+		});
+		return deferred.promise;
+	},
+	getObjects: function (className) {
+		var deferred = Q.defer();
+		parse.getObjects(className, function(err, response, body, success) {
+			if (err) {
+				deferred.reject(err);
+			} else if (body.error) {
+				deferred.reject(body);
+			} else {
+				deferred.resolve(body);
+			}
+		});
+		return deferred.promise;
+	},
+	addObject: function (className, newData) {
+		var deferred = Q.defer();
+		parse.createObject(className, newData, function(err, response, body, success) {
+			if (err) {
+				deferred.reject(err);
+			} else if (body.error) {
+				deferred.reject(body);
+			} else {
+				deferred.resolve(body);
+			}
+		});
+		return deferred.promise;
+	},
+	updateObject: function (className, id, updateData) {
+		var deferred = Q.defer();
+		parse.updateObject(className, id, updateData, function(err, response, body, success) {
+			if (err) {
+				deferred.reject(err);
+			} else if (body.error) {
+				deferred.reject(body);
+			} else {
+				deferred.resolve(body);
+			}
+		});
+		return deferred.promise;
+	},
+	deleteObject: function (className, id) {
+		var deferred = Q.defer();
+		parse.deleteObject(className, id, function(err, response, body, success) {
+			if (err) {
+				deferred.reject(err);
+			} else if (body.error) {
+				deferred.reject(body);
+			} else {
+				deferred.resolve(body);
+			}
+		});
+		return deferred.promise;
+	}
+};
+
--- api/routes/wines.js
+++ api/routes/wines.js
@@ -1,90 +1,63 @@
-var Kaiseki = require('../lib/kaiseki');
+var p = require('../lib/parse-REST-functions');
 
-// instantiate
-var APP_ID = 'uX77K4d7z4PnWODj6Rr1YkJwAIsFHQp4aMeDa4mm';
-var REST_API_KEY = 'xIfE9g6vDdZMvA6nYxF0kfuVnbyswUO2AlY159T7';
-
-var parse = new Kaiseki(APP_ID, REST_API_KEY);
 var className = 'wines';
 
 exports.findAll = function(req, res) {
-	// res.send([{name:'wine1'}, {name:'wine2'}, {name:'wine3'}]);
 
-	parse.getObjects('wines', function(err, response, body, success) {
-		if (err) {
+	p.getObjects('wines')
+		.then(function(data) {
+			res.send(data);
+		}, function(data) {
 			res.writeHead(500, {'Content-Type': 'text/plain'});
 			res.send('error: ' + err);
-		} else {
-			console.log('all wines = ', body);
-			res.send(body);
-		}
-	});
+		});
 };
 
 exports.findById = function(req, res) {
-	var id = req.params.id;
-	console.log('Retrieving wine: ' + id);
 
-	var queryParams = {};
-	
-	parse.getObject(className, id, queryParams, function(err, response, body, success) {
-		
-		if (err) {
+	var id = req.params.id;
+	p.getObject('wines', id)
+		.then(function(data) {
+			res.send(data);
+		}, function(data) {
 			res.writeHead(500, {'Content-Type': 'text/plain'});
 			res.send('error: ' + err);
-		} else {
-			console.log('object retrieved = ', body);
-			res.send(body);
-		}
-	});
-}
+		});
+};
 
 exports.addWine = function(req, res) {
 	var wine = req.body;
-	console.log('Adding wine: ' + JSON.stringify(wine));
-	
-	parse.createObject(className, wine, function(err, response, body, success) {
-		
-		if (err) {
+
+	p.addObject('wines', wine)
+		.then(function(data) {
+			res.send(data);
+		}, function(data) {
 			res.writeHead(500, {'Content-Type': 'text/plain'});
 			res.send('error: ' + err);
-		} else {
-			console.log('object created = ', body);
-			console.log('object id = ', body.objectId);
-			res.send(body);
-		}
-	});
-}
+		});
+};
 
 exports.updateWine = function(req, res) {
 	var wine = req.body;
 	var id = req.params.id;
-	console.log('Updating wine: ' + JSON.stringify(wine));
-	
-	parse.updateObject(className, id, wine, function(err, response, body, success) {
-		
-		if (err) {
+
+	p.updateObject('wines', id, wine)
+		.then(function(data) {
+			res.send(data);
+		}, function(data) {
 			res.writeHead(500, {'Content-Type': 'text/plain'});
 			res.send('error: ' + err);
-		} else {
-			console.log('object updated = ', body);
-			res.send(body);
-		}
-	});
-}
+		});
+};
 
 exports.deleteWine = function(req, res) {
 	var id = req.params.id;
-	console.log('Deleting wine: ' + id);
 	
-	parse.deleteObject(className, id, function(err, response, body, success) {
-		
-		if (err) {
+	p.deleteObject('wines', id)
+		.then(function(data) {
+			res.send({ success: true, objectId: id });
+		}, function(data) {
 			res.writeHead(500, {'Content-Type': 'text/plain'});
 			res.send('error: ' + err);
-		} else {
-			console.log('object deleted = ', body);
-			res.send({ success: true, objectId: id });
-		}
-	});
-}
+		});
+};
